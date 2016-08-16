c669e3440c7ede9a586193443f140058f14a174f
Rounak Banik <rounakbanik@gmail.com>
2016-08-17 00:02:10 +0530

    Remove comments, console.log and unnecessary files.

--- contentscript.js
+++ contentscript.js
@@ -1,40 +1,3 @@
-console.log("hello, world");
-
-/*var elem = null;
-$("*").hover(function() {
-	elem = this;
-	console.log("Hello "+ elem);
-
-	var styleProps = $(this).css([
-    	"width", "height", "color", "background-color", "font-family", "font-size", "letter-spacing", "color", "display"
-  	]);
-	$.each( styleProps, function( prop, value ) {
-    	console.log( prop + ": " + value );
-  	});
-
-});*/
-
-/*$('*').qtip({ // Grab some elements to apply the tooltip to
-    content: {
-        text: function() {
-        	var styleProps = $(this).css([
-    			"width", "height", "color", "background-color", "font-family", "font-size", "font-weight", "letter-spacing", "color", "display"
-  			]);
-  			var texter = "<h3>CSS Properties</h3><br><ul>";
-  			$.each( styleProps, function( prop, value ) {
-    			texter += "<li><b>" + prop + ":</b> " + value + "</li>";
-  			});
-  			texter += "</ul>"
-        	return texter;
-        },
-    },
-    position: {
-        target: 'mouse', // Track the mouse as the positioning target
-        adjust: { x: 5, y: 5 } // Offset it slightly from under the mouse
-    }
-
-});*/
-
 $('p, a, h3, h1, h2, h4, h5, h6, blockquote, li, span, b, i, em, strong').qtip({ 
     show: { when: { event: 'click', } },// Grab some elements to apply the tooltip to
     content: {
@@ -51,8 +14,8 @@ $('p, a, h3, h1, h2, h4, h5, h6, blockquote, li, span, b, i, em, strong').qtip({
         },
     },
     position: {
-        target: 'mouse', // Track the mouse as the positioning target
-        adjust: { x: 5, y: 5 } // Offset it slightly from under the mouse
+        target: 'mouse', 
+        adjust: { x: 5, y: 5 } 
     }
 
 });
