a81be08a67832845b9485fc720c787723269371a
Rounak Banik <rounakbanik@gmail.com>
2016-08-16 13:17:14 +0530

    Create CSS Helper for all DOM elements.

---
+++ contentscript.js
@@ -0,0 +1,40 @@
+console.log("hello, world");
+
+/*var elem = null;
+$("*").hover(function() {
+	elem = this;
+	console.log("Hello "+ elem);
+
+	var styleProps = $(this).css([
+    	"width", "height", "color", "background-color", "font-family", "font-size", "letter-spacing", "color", "display"
+  	]);
+	$.each( styleProps, function( prop, value ) {
+    	console.log( prop + ": " + value );
+  	});
+
+});*/
+
+$('*').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+        text: function() {
+        	var styleProps = $(this).css([
+    			"width", "height", "color", "background-color", "font-family", "font-size", "letter-spacing", "color", "display"
+  			]);
+  			var texter = "<h2>CSS Properties</h2><ul>";
+  			$.each( styleProps, function( prop, value ) {
+    			texter += "<li><b>" + prop + ":</b> " + value + "</li>";
+  			});
+  			texter += "</ul>"
+        	return texter;
+        },
+    },
+    position: {
+        target: 'mouse', // Track the mouse as the positioning target
+        adjust: { x: 5, y: 5 } // Offset it slightly from under the mouse
+    }
+
+});
+
+var tester = function() {
+	return "Default";
+}
