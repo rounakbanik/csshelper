08cca6e0396ba273df5265c85f25d10370d72e44
Rounak Banik <rounakbanik@gmail.com>
2016-08-18 03:10:05 +0530

    Add toggle feature to enable/disable extension.

--- contentscript.js
+++ contentscript.js
@@ -1,22 +1,37 @@
-$('p, a, h3, h1, h2, h4, h5, h6, blockquote, li, span, b, i, em, strong').qtip({ 
-    show: { when: { event: 'click', } },// Grab some elements to apply the tooltip to
-    content: {
-        text: function() {
-          var styleProps = $(this).css([
-          "font-family", "font-size", "font-weight", "font-style", "color", "letter-spacing", "line-height", "text-decoration"
-        ]);
-        var texter = "";
-        $.each( styleProps, function( prop, value ) {
-          texter += "<b>" + prop + ":</b> " + value + "<br>";
-        });
-        //texter += ""
-          return texter;
-        },
-    },
-    position: {
-        target: 'mouse', 
-        adjust: { x: 5, y: 5 } 
-    }
+var check= false;
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    check = !check;
+    console.log(check);
+    
+    if(check) {
+      var Toolqtip = $("p, a, h3, h1, h2, h4, h5, h6, blockquote, li, span, b, i, em, strong").qtip({ 
+          show: { when: { event: 'click', } },
+          content: {
+            text: function() {
+                  var styleProps = $(this).css([
+                    "font-family", "font-size", "font-weight", "font-style", "color", "letter-spacing", "line-height", "text-decoration"
+                  ]);
+            var texter = "";
+            $.each( styleProps, function( prop, value ) {
+              texter += "<b>" + prop + ":</b> " + value + "<br>";
+            });
+            return texter;
+          },
+        },
+        position: {
+          target: 'mouse', 
+          adjust: { x: 5, y: 5 } 
+        }
+    });
+  }
+    
+  else {
+    $("p, a, h3, h1, h2, h4, h5, h6, blockquote, li, span, b, i, em, strong").qtip('destroy', false) 
+  }
 });
+
+
+
 
